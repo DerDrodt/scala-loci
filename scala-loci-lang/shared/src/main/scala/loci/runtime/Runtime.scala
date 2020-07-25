6a0700eeda31a69ac74de9522d6c59d3127623fd
Daniel <d.drodt@gmx.de>
2020-07-25 16:59:03 +0200

    make Rutime.remoteConnections public

--- scala-loci-lang/shared/src/main/scala/loci/runtime/Runtime.scala
+++ scala-loci-lang/shared/src/main/scala/loci/runtime/Runtime.scala
@@ -24,7 +24,7 @@ class Runtime[P](
 
   private val doTerminated = Notice.Steady[Unit]
 
-  private val remoteConnections = new RemoteConnections(peer, ties)
+  public val remoteConnections = new RemoteConnections(peer, ties)
 
   private object state {
     private var running = false
